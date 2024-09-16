dca3c1b4792763419165fe7acf26d9414ef0b54b
Seung-Woo Seo <seungwoo103@gmail.com>
2024-09-16 03:17:51 -0400

    studied functions, looping independently. still not 100% sure but have much better understanding at this point to move on

--- froyo.js
+++ froyo.js
@@ -4,20 +4,37 @@
 /**
 * @param {string[]} flavors
 */
-
+//indepent study on looping and object but didn't get the same final result, for now I'm doing what Lester showed us in class but i do have solid understanding of basic functions and looping
 function countFlavors(flavors) {
-const counts = {};
-for (const flavor of flavors) {
-    if (flavor in counts) {
+    const counts = {};
+    for (const flavor of flavors) {
+      if (flavor in counts) {
         counts[flavor] += 1;
-    } else {
+      } else {
         counts[flavor] = 1;
+      }
     }
-}
-return counts;
-}
-
-const userFlavors = prompt(
-    "Enter list of froyo flavors","vanilla,vanilla,vanilla,strawberry,coffee,coffee");
-
-    
+    return counts;
+  }
+  
+  function countFlavorsReduce(flavors) {
+    return flavors.reduce((counts, flavor) => {
+      if (flavor in counts) {
+        counts[flavor] += 1;
+      } else {
+        counts[flavor] = 1;
+      }
+      return counts;
+    }, {});
+  }
+  
+  const userFlavors = prompt(
+    "Please enter a comma-separated list of froyo flavors.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  );
+  
+  // I need to convert userFlavors from a string into an array of strings
+  const userFlavorsArray = userFlavors.split(",");
+  
+  const counts = countFlavors(userFlavorsArray);
+  console.log(counts);
